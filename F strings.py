6759a7dc1ab4be5a1c4647f5f77cf42780a413f8
Bhavesh Pratap singh <124441977+Bhaveshcode07@users.noreply.github.com>
2023-04-12 18:20:24 +0530

    Add files via upload
    In Python , we can use a type of string called f-string that helps us format our strings much more easily.

---
+++ F strings.py
@@ -0,0 +1,10 @@
+#we can use a type of string called f-string that helps us format our strings much more easily.#
+
+first_name = "Bhavesh"
+favourite_language = "python"
+
+#To define an f-string, we just add an f before the single or double quotes. Then, within the string, we surround the variables or expressions with curly braces {}.#
+
+print(f"Hi I am {first_name}.I am learing {favourite_language}")
+
+#output = Hi I am Bhavesh. I am learing python #
